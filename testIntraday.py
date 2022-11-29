6894c969f5a44383e4da43d50d29fb1aa8cde5dc
Rishabh Iyer <rishabhnad@gmail.com>
2022-11-29 13:52:20 +0530

    Update testIntraday

--- testIntraday.py
+++ testIntraday.py
@@ -22,7 +22,8 @@ trade = pd.DataFrame()
 trades = []
 generalconfig = {"SquareOffSL":defs.ONELEG,"SquareOffTG":defs.ONELEG,
                      "EnterTime":datetime.time(9,30),"ExitTime":datetime.time(15,15), "symbol":"BANKNIFTY",
-                     "ReEntrySL": defs.NO, "ReEntryTG": defs.NO, "debug": defs.DEBUGTIME}
+                     "ReEntrySL": defs.NO, "ReEntryTG": defs.NO, "debug": defs.DEBUGTIME, "TimeRenter": defs.NO,
+                 "ReEnterEvery": 5}
 positionconfig = [{"Type":defs.CALL,"Action":defs.SELL,"Delta":0, "SLPc":25, "TargetPc":50, "LotSize":1,
                        "SL": defs.YES, "Target":defs.NO},
                       {"Type":defs.PUT,"Action":defs.SELL,"Delta":0,"SLPc":25,"TargetPc":50,"LotSize":1,
