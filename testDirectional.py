0b7a7562a3745a65d44b9b3bac333c80a888fa4c
ShadyKid <35014054+drnerd47@users.noreply.github.com>
2022-12-13 13:54:20 +0530

    minor edits

--- testDirectional.py
+++ testDirectional.py
@@ -14,8 +14,13 @@ import TIconfigs
 
 warnings.filterwarnings("ignore")
 
+<<<<<<< Updated upstream
 start_date = datetime.date(2022, 1, 1)
 end_date = datetime.date(2022, 9, 30)
+=======
+start_date = datetime.date(2018, 1, 1)
+end_date = datetime.date(2022, 9, 10)
+>>>>>>> Stashed changes
 delta = datetime.timedelta(days=1)
 
 user = "RI"
@@ -34,6 +39,7 @@ Banknifty_Path = Root + "NIFTYOptionsData/OptionsData/Banknifty/"
 Nifty_Path = Root + "NIFTYOptionsData/OptionsData/Nifty/"
 
 
+<<<<<<< Updated upstream
 #generalconfig = genconfigs.generalconfigBNST
 #generalconfig = genconfigs.generalconfigNRSIADX
 #generalconfig = genconfigs.generalconfigNST
@@ -45,6 +51,19 @@ positionconfig = posconfigs.positionconfigsinglebuydirec
 TIconfig = TIconfigs.TIconfigBB2
 #TIconfig = TIconfigs.TIconfigST
 
+=======
+# #generalconfig = genconfigs.generalconfigBNST
+# generalconfig = genconfig.generalconfigBNRSIADX
+# #generalconfig = genconfigs.generalconfigBNBB
+# #positionconfig = posconfigs.positionconfigsingleselldirecSL
+# positionconfig = posconfig.positionconfigsingleselldire
+# TIconfig = TIconfigs.TIconfigRSI_ADX
+# #TIconfig = TIconfigs.TIconfigBB2
+# #TIconfig = TIconfigs.TIconfigST
+generalconfig = genconfig.generalconfigNRSIADX
+positionconfig = posconfig.positionconfigsinglebuydirec
+TIconfig = TIconfigs.TIconfigRSI_ADX
+>>>>>>> Stashed changes
 
 
 if (generalconfig["symbol"] == defs.N):
