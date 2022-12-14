fc85654d883c5c646c075d4ec04741956b0bc956
Rishabh Iyer <rishabhnad@gmail.com>
2022-12-14 23:51:14 +0530

    utils sell margin calculator

--- utils.py
+++ utils.py
@@ -7,3 +7,20 @@ def BuyMarginCalculator(trades, symbol):
   elif symbol == defs.N :
     margin = trades.EnterPrice * defs.NLOTSIZE
   return (margin.max(), margin.mean(), margin.min())
+
+def SellMarginCalculator(positiontype, numcalllegs, numputlegs, symbol):
+  if (positiontype == "Naked"):
+    if (symbol == defs.BN):
+      singlecost = 150000
+      doublecost = 180000
+    elif (symbol == defs.N):
+      singlecost = 125000
+      doublecost = 110000
+  elif (positiontype == "Hedged"):
+    if (symbol == defs.BN):
+      singlecost = 45000
+      doublecost = 70000
+    elif (symbol == defs.N):
+      singlecost = 30000
+      doublecost = 60000
+  return min(numcalllegs, numputlegs)*doublecost + (max(numcalllegs, numputlegs) - min(numcalllegs, numputlegs))*singlecost
