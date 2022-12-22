f4d991a8b90bd7b63f9f48db736428091830f2e0
ShadyKid <35014054+drnerd47@users.noreply.github.com>
2022-12-22 19:50:19 +0530

    imp minor fixes

--- directional.py
+++ directional.py
@@ -8,7 +8,8 @@ import supertrend as st
 import numpy as np
 
 # This function resamples data to required frequency from 1 min data
-def Resample(df, freq='1T'): # freq format,  for 2min freq='2T', for 3min freq='3T' 
+def Resample(df, f): # freq format,  for 2min freq='2T', for 3min freq='3T'
+    freq = str(f)+"T" 
     resample_df = df.resample(freq, origin='end').agg({
     'open':'first',
     'high':'max',
