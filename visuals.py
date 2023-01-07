5e9b4ec37c2057f6389287e4840d79ac68c97740
ShadyKid <35014054+drnerd47@users.noreply.github.com>
2023-01-07 19:32:22 +0530

    minor edits

--- visuals.py
+++ visuals.py
@@ -342,14 +342,14 @@ def plotTrade(trade, signaldata, datapath):
             y = df.upband,
             mode = "lines",
             name = "Upper Bollinger Band",
-            marker = {'color' : 'yellow'}
+            marker = {'color' : 'brown'}
         ), row = 1, col = 1) 
         fig.add_trace(go.Line(
             x = df.index,
             y = df.lowband,
             mode = "lines",
             name = "Lower Bollinger Band",
-            marker = {'color' : 'yellow'}
+            marker = {'color' : 'brown'}
         ), row = 1, col = 1)
     
     else:
